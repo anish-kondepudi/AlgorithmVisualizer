10f05c731454762a34235f4e4f8416902774050e
ian-chuang <iantc104@gmail.com>
2021-07-31 21:58:06 -0700

    changed to forwardRef much faster

--- algorithm_visualizer/src/pages/graph_components/Node.js
+++ algorithm_visualizer/src/pages/graph_components/Node.js
@@ -1,35 +1,17 @@
-import React, {Component} from 'react';
+import { useState, useEffect, useRef, forwardRef } from "react"
 import "./Node.css";
 
-export default class Node extends Component {
+export const Node = forwardRef((props, ref) => {
+  const {row, col} = props;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      type: this.props.type
-    };
-
-    this.dv = Infinity;
-    this.known = false;
-    this.pv = null;
-    this.row = this.props.row;
-    this.col = this.props.col;
-  }
-
-  reset() {
-    this.dv = Infinity;
-    this.known = false;
-    this.pv = null;
-  }
-
-  render() {
-    return (
-      <div className="wrapper">
-        <div
-          id={`node-${this.row}-${this.col}`}
-          className={`node node-${this.state.type}`}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="wrapper">
+      <div
+        ref={ref}
+        id={`node-${row}-${col}`}
+        className={'node-empty'}
+      />
+    </div>
+  );
+  
+});
