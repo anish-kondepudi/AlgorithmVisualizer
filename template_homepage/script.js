c4624df00d9d25f83c24d2c140eac6d22d510e4a
Anish Kondepudi <anish.kondepudi@gmail.com>
2021-08-11 18:44:53 -0700

    Deleted template homepage

--- template_homepage/script.js
+++
@@ -1,9 +0,0 @@
-var canvasDiv = document.getElementById('particle-canvas');
-var options = {
-  particleColor: '#e37575',
- 
-  interactive: true,
-  speed: 'medium',
-  density: 'high'
-};
-var particleCanvas = new ParticleNetwork(canvasDiv, options);
