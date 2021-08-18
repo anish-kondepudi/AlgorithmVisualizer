61f0e422769a285f13267b515c99c0262861d182
ian-chuang <iantc104@gmail.com>
2021-08-17 17:35:38 -0700

    fix bug

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,18 +6,24 @@ export const LandingPage = () => {
   const history = useHistory()
 
   return (
-    <div className="landingPage d-flex align-items-center justify-content-center p-3">
-      <div className="w-100 d-flex justify-content-around flex-wrap gap-4 m-auto">
+    <div className="landingPage d-flex flex-column pt-5">
+        <div className="w-100 flex-grow-1 d-flex justify-content-around align-items-center flex-wrap gap-4 m-auto">
           <div onClick={() =>  history.push('/sorting')} className="algo">
             <h2 className="mb-4">Sorting Algorithms</h2>
-            <img alt="sorting" src={process.env.PUBLIC_URL + "/sorting_example.PNG"} />
-            
+            <img src={process.env.PUBLIC_URL + "/sorting_example.PNG"} />
           </div>
 
           <div onClick={() =>  history.push('/graph')} className="algo">
             <h2 className="mb-4">Graph Algorithms</h2>
-            <img alt="graph" src={process.env.PUBLIC_URL + "/graph_example.PNG"} />
-            
+            <img src={process.env.PUBLIC_URL + "/graph_example.PNG"} />
+          </div>
+        </div>
+        <div className="mb-2 ms-2">
+          <div 
+            className="github-link d-inline-block" 
+            onClick={() => {window.location.href="https://github.com/anish-kondepudi/AlgorithmVisualizer"}}
+          > 
+            <i class="fab fa-3x fa-github"></i>
           </div>
         </div>
     </div>
